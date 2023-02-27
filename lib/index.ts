cd537608c57a3451589c3f3adb3fd62b7fb04330
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-02-27 13:27:44 +1100

    feat: add experimental support for n3 syntax

--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,9 @@
-import { DataFactory as DF } from 'n3';
 import type { Quad } from '@rdfjs/types';
 import Store from './volatile-store';
-import { TTLWriter } from './ttlwriter';
+import { TTLWriter, Options } from './ttlwriter';
 import Writer from './writer';
 
-export interface Options {
-  prefixes: Record<string, string>
-}
+export { Options };
 
 export async function write(quads: Quad[], options?: Options): Promise<string> {
   // eslint-disable-next-line no-async-promise-executor
@@ -15,16 +12,6 @@ export async function write(quads: Quad[], options?: Options): Promise<string> {
       let s = '';
       const volatileStore = new Store(quads);
 
-      const graphs = volatileStore.getGraphs(null, null, null);
-
-      if (graphs.length > 1) {
-        throw new Error('More than one graph found - can serialize in the default graph');
-      }
-
-      if (graphs.length === 1 && !graphs[0].equals(DF.defaultGraph())) {
-        throw new Error(`Expected all triples to be in the default graph, instead triples were in ${graphs[0].value}`);
-      }
-
       const w = new Writer({
         write: (chunk: string) => {
           s += chunk;
@@ -32,11 +19,13 @@ export async function write(quads: Quad[], options?: Options): Promise<string> {
         end: () => {
           resolve(s);
         },
+        compact: options?.compact,
       });
       const writer = new TTLWriter(
         volatileStore,
         w,
         options?.prefixes,
+        options,
       );
       await writer.write();
     } catch (e) {
